7e6fd22f8fb8c8e7f550bef4e8e6e084db56b5a5
s74588 <bschurian4@gmail.com>
2018-11-07 06:34:47 +0100

    isy02-1 100%

--- isy-02/02_features.py
+++ isy-02/02_features.py
@@ -1,7 +1,7 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-cv2.namedWindow('Interactive Systems: Towards AR Tracking')
+# cv2.namedWindow('Interactive Systems: Towards AR Tracking')
 while True:
 
     # 1. read each frame from the camera (if necessary resize the image)
@@ -12,4 +12,21 @@ while True:
 
     # close the window and application by pressing a key
 
-    # YOUR CODE HERE
+    # code taken from: https://docs.opencv.org/3.1.0/da/df5/tutorial_py_sift_intro.html
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp = sift.detect(gray, None)
+    outImage = None
+    # img = cv2.drawKeypoints(gray, kp, outImage)
+    img = cv2.drawKeypoints(gray, kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS, outImage=img)
+    cv2.imshow('stream', img)
+
+
+    # termination criteria
+    ch = cv2.waitKey(1) & 0xFF
+    if ch == ord('q'):
+        break
+
+
+cv2.destroyAllWindows()
