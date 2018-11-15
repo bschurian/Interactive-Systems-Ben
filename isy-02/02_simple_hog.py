90b337f784bd0089ad12cf25274cb20f58ba160c
s74588 <bschurian4@gmail.com>
2018-11-15 15:40:29 +0100

    isy-02:A4 Hog 100%

--- isy-02/02_simple_hog.py
+++ isy-02/02_simple_hog.py
@@ -3,6 +3,7 @@ import cv2
 import math
 import matplotlib
 import glob
+import math
 
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -15,14 +16,15 @@ import matplotlib.pyplot as plt
 ###############################################################
 
 
-def plot_histogram(hist, bins):
+def plot_histogram(hist, bins,filename):
     width = 0.7 * (bins[1] - bins[0])
     center = (bins[:-1] + bins[1:]) / 2
+
     plt.bar(center, hist, align='center', width=width)
     plt.show()
 
 
-def compute_simple_hog(imgcolor, keypoints):
+def compute_simple_hog(imgcolor, keypoints,filename):
     # convert color to gray image and extract feature in gray
     imggray = cv2.cvtColor(imgcolor, cv2.COLOR_BGR2GRAY)
 
@@ -33,7 +35,9 @@ def compute_simple_hog(imgcolor, keypoints):
     ySobel = cv2.Sobel(imggray, cv2.CV_64F, 0, 1, ksize=5)
 
     # compute magnitude and angle of the gradients
-    magnitudes, angles = cv2.cartToPolar(xSobel, ySobel, angleInDegrees=False)
+    # magnitudes, angles = cv2.cartToPolar(xSobel, ySobel, angleInDegrees=False)
+    magnitudes = cv2.magnitude(xSobel,ySobel)
+    angles = cv2.phase(xSobel,ySobel,angleInDegrees=False)
     # print(magnitudes, angles)
 
     # go through all keypoints and and compute feature vector
@@ -53,15 +57,14 @@ def compute_simple_hog(imgcolor, keypoints):
         # answer to that question use np.histogram
         usefull_kp_angles = [angle for (angle, magnitude) in zip(kp_angles.flatten(), kp_magnitudes.flatten()) if
                              magnitude != 0]
-        (hist, bins) = np.histogram(usefull_kp_angles, bins=8, range=(0.0, 7.0))
+        (hist, bins) = np.histogram(usefull_kp_angles, bins=8, range=(0.0, 2*math.pi),density=True)
 
-        plot_histogram(hist, bins)
+        plot_histogram(hist, bins,filename)
 
         descr[count] = hist
 
         count += 1
     return descr
-
 
 keypoints = [cv2.KeyPoint(15, 15, 11)]
 
@@ -70,6 +73,5 @@ res = []
 filenames = glob.glob('./images/hog_test/*.jpg')
 for filename in filenames:
     img = cv2.imread(filename)
-    cv2.imshow("matches.jpg", img)
-    res.append(compute_simple_hog(img, keypoints))
+    res.append(compute_simple_hog(img, keypoints,filename))
 cv2.destroyAllWindows()
