45d40717d1cf8aa142a402d95714fcf6c4e06494
s74588 <bschurian4@gmail.com>
2018-12-04 18:05:35 +0100

    isy03-A02 100%

--- isy-03/03_kmeans.py
+++ isy-03/03_kmeans.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import math
 import sys
 
 
@@ -12,8 +11,8 @@ import sys
 
 # implement distance metric - e.g. squared distances between pixels
 def distance(a, b):
-    pass
-    # YOUR CODE HERE
+    return np.linalg.norm(a - b)
+
 
 # k-means works in 3 steps
 # 1. initialize
@@ -23,8 +22,20 @@ def distance(a, b):
 
 def update_mean(img, clustermask):
     """This function should compute the new cluster center, i.e. numcluster mean colors"""
-    # YOUR CODE HERE
-    pass
+    flat = img.flatten()
+    flat.reshape((int(flat.shape[0] / 3), 3))
+    w, h, _ = clustermask.shape
+    cluster_assignees={}
+    for cid,_ in enumerate(current_cluster_centers):
+        cluster_assignees[cid] = []
+    for x in range(w):
+        for y in range(h):
+            cid = clustermask[x, y][0]
+            cluster_assignees[cid].append(img[x,y])
+    for cid, pixels in cluster_assignees.items():
+        current_cluster_centers[cid] = np.mean(np.array(pixels),axis=0)
+    return clustermask
+
 
 def assign_to_current_mean(img, result, clustermask):
     """The function expects the img, the resulting image and a clustermask.
@@ -33,15 +44,37 @@ def assign_to_current_mean(img, result, clustermask):
     Return: the overall error (distance) for all pixels to there closest cluster center (mindistance px - cluster center).
     """
     overall_dist = 0
-    # YOUR CODE HERE
+    w, h, _ = img.shape
+    for x in range(w):
+        for y in range(h):
+            ipixel = img[x, y]
+            dists = {}
+            for i, c in enumerate(current_cluster_centers):
+                dists[i] = distance(ipixel, c)
+            cid, dist = min(dists.items(), key=lambda d: d[1])
+            clustermask[x, y] = cid
+            result[x, y] = current_cluster_centers[cid]
+            overall_dist += dist
     return overall_dist
 
-
+def refill_real(img, result, clustermask, cluster_colors):
+    """fill real with the vals of cluster_colors
+    """
+    overall_dist = 0
+    w, h, _ = img.shape
+    for x in range(w):
+        for y in range(h):
+            cid = clustermask[x, y]
+            print(cluster_colors[cid], result[x, y])
+            result[x, y] = cluster_colors[cid]
 
 def initialize(img):
     """inittialize the current_cluster_centers array for each cluster with a random pixel position"""
-    # YOUR CODE HERE
-    print(current_cluster_centers)
+    w, h, _ = img.shape
+    for c in current_cluster_centers:
+        x = np.random.randint(w)
+        y = np.random.randint(h)
+        c[:] = img[x, y]
 
 
 def kmeans(img):
@@ -60,16 +93,22 @@ def kmeans(img):
     # initializes each pixel to a cluster
     # iterate for a given number of iterations or if rate of change is
     # very small
-    # YOUR CODE HERE
-
+    initialize(img)
+    i = 0
+    while i < max_iter and dist > max_change_rate:
+        assign_to_current_mean(img, result, clustermask)
+        clustermask = update_mean(img, clustermask)
+        i += 1
+    refill_real(img, result, clustermask, cluster_colors)
     return result
+
 
 # num of cluster
 numclusters = 3
 # corresponding colors for each cluster
-cluster_colors = [[255, 0, 0], [0, 255, 0], [0, 0, 255], [0, 255, 255], [255, 255, 255], [0, 0, 0], [128, 128, 128]]
+cluster_colors = np.array([[255, 0, 0], [0, 255, 0], [0, 0, 255], [0, 255, 255], [255, 255, 255], [0, 0, 0], [128, 128, 128]],np.uint8)
 # initialize current cluster centers (i.e. the pixels that represent a cluster center)
-current_cluster_centers = np.zeros((numclusters, 1, 3), np.float32)
+current_cluster_centers = np.zeros((numclusters, 3), np.uint8)
 
 # load image
 imgraw = cv2.imread('./images/Lenna.png')
@@ -92,6 +131,9 @@ vis = np.zeros((max(h1, h2), w1 + w2, 3), np.uint8)
 vis[:h1, :w1] = res
 vis[:h2, w1:w1 + w2] = image
 
-cv2.imshow("Color-based Segmentation Kmeans-Clustering", vis)
-cv2.waitKey(0)
+while True:
+    cv2.imshow("Color-based Segmentation Kmeans-Clustering", vis)
+    ch = cv2.waitKey(1) & 0xFF
+    if ch == ord('q'):
+        break
 cv2.destroyAllWindows()
