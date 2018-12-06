53550bab0fd18652b8dd6fc3e7094fea939c54ab
s74588 <bschurian4@gmail.com>
2018-12-07 00:29:52 +0100

    looking okay, but still some kinda z-flip?

--- isy-03/03_ar.py
+++ isy-03/03_ar.py
@@ -21,8 +21,9 @@ keypointsMarker, descriptorsMarker = sift.detectAndCompute(marker, None)
 
 def render_virtual_object(img, x_start, y_start, x_end, y_end, quad):
     # define vertices, edges and colors of your 3D object, e.g. cube
-    vertices = np.float32([[0, 0, 0], [0, 0, 1], [1, 0, 1], [1, 0, 0], [0, 1, 0], [1, 1, 1], [1, 1, 1], [1, 1, 0]])
-    edges = [(0, 1), (2, 3), (4, 5), (6, 7), (0, 2), (1, 3), (4, 6), (5, 7), (0, 4), (1, 5), (2, 6), (3, 7)]
+    vertices = np.float32([[0, 0, 0], [0, 0, 1], [1, 0, 1], [1, 0, 0],
+                           [0, 1, 0], [0, 1, 1], [1, 1, 1], [1, 1, 0]])
+    edges = [(0, 1), (2, 3), (4, 5), (6, 7), (0, 4), (1, 5), (2, 6), (3, 7), (0, 3), (1, 2), (5, 6), (4, 7)]
 
     color_lines = (0, 0, 0)
 
